d578bd88ed213ff76bf6eaeef80088e8aba2f675
Khawaja Abdul Moiz <156238498+KhawajaAbdulMoiz@users.noreply.github.com>
2024-05-07 23:34:24 +0500

    Basic Practice Tasks for Loops in C++

---
+++ Loops/Swapping_Numbers.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,temp,b;
+	cout<<"====> SWAPPING NUMBERS <===="<<endl;
+	cout<<"Enter num 1 : ";
+	cin>>a;
+	cout<<"Enter num 2 : ";
+	cin>>b;
+	cout<<"Numbers before Swap "<<a<<" and "<<b<<endl;
+	temp=a;
+	a=b;
+	b=temp;
+	cout<<"Numbers after Swap "<<a<<" and "<<b;
+	
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	int num1,num2,sum,mul,sub,div;
+//	int choice;	
+//	cout<<"Press + for Finding Area"<<endl;
+//	cout<<"Press 2 for Finding Circumference"<<endl;
+//	cout<<"Press 3 for Finding "<<endl;
+//	cout<<"Press 4 for Quotient"<<endl;
+//	cout<<"Enter Your Choice : ";
+//	cin>>choice;
+//	switch(choice){
+//	case 1:
+//		cout<<"Entr Num1  : ";
+//		cin>>num1;
+//		cout<<"Entr Num1  : ";
+//		cin>>num2;
+//		sum=num1+num2;
+//		cout<<sum;
+//		break;
+//	case 2:
+//		cout<<"Entr Num1  : ";
+//		cin>>num1;
+//		cout<<"Entr Num1  : ";
+//		cin>>num2;
+//		sub=num1-num2;
+//		cout<<sub;
+//		break;
+//	case 3:
+//		cout<<"Entr Num1  : ";
+//		cin>>num1;
+//		cout<<"Entr Num1  : ";
+//		cin>>num2;
+//		mul=num1*num2;
+//		cout<<mul;
+//		break;
+//	case 4:
+//		cout<<"Entr Num1  : ";
+//		cin>>num1;
+//		cout<<"Entr Num1  : ";
+//		cin>>num2;
+//		div=num1/num2;
+//		cout<<div;
+//		break;
+//	default:
+//			cout<<"Enter a Valid Input";
+//
+//	}
+
+
+
+
